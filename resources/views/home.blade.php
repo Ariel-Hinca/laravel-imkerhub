2ddd7a07851801c2eb7e3192ec0a23ee5d48da2c
Ariel-Hinca <hincapieariel@gmail.com>
2025-12-03 11:46:01 +0100

    Create home page and update root route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ImkerHub</title>
+</head>
+<body>
+<h1>Welkom bij ImkerHub</h1>
+<p>Dit is het platform waar imkers en klanten elkaar vinden.</p>
+
+<p>
+    <a href="/login">Inloggen</a> |
+    <a href="/register">Registreren</a>
+</p>
+</body>
+</html>
