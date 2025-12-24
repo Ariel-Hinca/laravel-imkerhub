6377e8c3f54f84587f4bf40558a3cf09fa1d4eca
Ariel-Hinca <hincapieariel@gmail.com>
2025-12-24 10:07:04 +0100

    Seller middleware toegevoegd en geregistreerd

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\SellerMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         // hier aliasen we je middleware
         $middleware->alias([
             'admin' => AdminMiddleware::class,
+            'seller' => SellerMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
