f9b5bf2a3a473f2e773fdc7c9a4c662d392a0491
Ariel-Hinca <hincapieariel@gmail.com>
2025-12-25 10:54:52 +0100

    Update admin producten view

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,22 +1,32 @@
-<h1>Admin - Producten</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-white text-2xl">
+            Admin - Producten
+        </h2>
+    </x-slot>
+    <div class="text-white" style="padding-left: 150px;">
 
-@if($products->count() === 0)
-<p>Geen producten.</p>
-@else
-<ul>
-    @foreach($products as $product)
-    <li>
-        <strong>{{ $product->name }}</strong>
-        (€{{ $product->price }}) -
-        Verkoper: {{ $product->user->name ?? 'Onbekend' }}
+        @if($products->count() === 0)
+        <p>Geen producten.</p>
+        @else
+        <ul>
+            @foreach($products as $product)
+            <li>
+                <strong>{{ $product->name }}</strong>
+                (€{{ $product->price }}) -
+                Verkoper: {{ $product->user->name ?? 'Onbekend' }}
 
-        <form action="{{ route('admin.products.destroy', $product->id) }}" method="POST" style="display:inline;">
-            @csrf
-            <button type="submit">Verwijder</button>
-        </form>
-    </li>
-    @endforeach
-</ul>
-@endif
+                <form action="{{ route('admin.products.destroy', $product->id) }}" method="POST"
+                      style="display:inline;">
+                    @csrf
+                    <button type="submit" class="text-red-400">Verwijder </button>
+                </form>
+            </li>
+            @endforeach
+        </ul>
+        <br>
+        @endif
 
-<p><a href="/dashboard">Terug naar admin</a></p>
+        <p><a href="/dashboard" class="text-blue-400">Terug naar admin</a></p>
+    </div>
+</x-app-layout>
