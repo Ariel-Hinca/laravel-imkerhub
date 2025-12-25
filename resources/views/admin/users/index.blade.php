2a2cf8b7ebd73a59c9d0d4b73d745e1cdb0f6326
Ariel-Hinca <hincapieariel@gmail.com>
2025-12-25 10:50:57 +0100

    Update admin users view

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,20 +1,29 @@
-<h1>Admin - Gebruikers</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-white text-2xl">
+            Admin - Gebruikers
+        </h2>
+    </x-slot>
+    <div class="text-white" style="padding-left: 150px;">
 
-<ul>
-    @foreach($users as $user)
-    <li>
-        {{ $user->name }} ({{ $user->email }}) - role: {{ $user->role }}
+        <ul>
+            @foreach($users as $user)
+            <li>
+                {{ $user->name }} ({{ $user->email }}) - role: {{ $user->role }}
 
-        @if($user->email !== 'admin@ehb.be')
-        <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST" style="display:inline;">
-            @csrf
-            <button type="submit">Verwijder</button>
-        </form>
-        @else
-        (default admin)
-        @endif
-    </li>
-    @endforeach
-</ul>
+                @if($user->email !== 'admin@ehb.be')
+                <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST" style="display:inline;">
+                    @csrf
+                    <button type="submit" class="text-red-400">Verwijder</button>
+                </form>
+                @else
+                (default admin)
+                @endif
+            </li>
+            <br>
+            @endforeach
+        </ul>
 
-<p><a href="/dashboard">Terug naar dashboard</a></p>
+        <p><a href="/dashboard" class="text-blue-400">Terug naar dashboard</a></p>
+    </div>
+</x-app-layout>
