d67d78d3881f249509fc5a0e21b73da6129bfc1b
Ariel-Hinca <hincapieariel@gmail.com>
2025-12-25 13:25:53 +0100

    Update gewone user Read Me! view

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,13 +1,21 @@
-<h1>{{ $news->title }}</h1>
-<p>Publicatiedatum: {{ $news->published_at }}</p>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-white text-2xl">
+            {{ $news->title }}
+        </h2>
+    </x-slot>
+    <div class="text-white" style="padding-left: 150px;">
 
-@if($news->image)
-<p>
-    <img src="{{ asset('storage/' . $news->image) }}" width="300" alt="Nieuws afbeelding">
-</p>
-@endif
+        <p>Publicatiedatum: {{ $news->published_at }}</p>
+        <br>
+        @if($news->image)
+        <p>
+            <img src="{{ asset('storage/' . $news->image) }}" width="300" alt="Nieuws afbeelding">
+        </p>
+        @endif
 
-<p>{{ $news->content }}</p>
-
-<p><a href="{{ route('news.index') }}">← Terug naar alle nieuwtjes</a></p>
-
+        <p>{{ $news->content }}</p>
+        <br>
+        <p><a href="{{ route('news.index') }}" class="text-blue-400">Terug naar alle nieuwtjes</a></p>
+    </div>
+</x-app-layout>
